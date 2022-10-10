fb9b5b3f168f033f7d18651c76ea2208aa21e58a
NicBeans <nicholasbosman007@gmail.com>
2022-10-10 15:55:26 +0200

    styling more to a pokemon theme

--- components/pokeliststyle.js
+++ components/pokeliststyle.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 export const PokeList = styled.p`
   font-size: 2em;
   text-align: center;
-  color: yellow;
+  color: #FFCC00;
   text-shadow:
-   -0.3px -0.3px 0 #000,  
-   0.3px -0.3px 0 #000,
-    -0.3px 0.3px 0 #000,
-    0.3px 0.3px 0 #000;
+   -0.8px -0.8px 0 #000,  
+   0.8px -0.8px 0 #000,
+    -0.8px 0.8px 0 #000,
+    0.8px 0.8px 0 #000;
 `;
--- components/titlestyle.js
+++ components/titlestyle.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Title = styled.h1`
   font-size: 4em;
   text-align: center;
-  color: gold;
+  color: #FFCC00;
   text-shadow:
    -1px -1px 0 #000,  
     1px -1px 0 #000,
@@ -15,7 +15,7 @@ export const Title = styled.h1`
 export const SubTitle = styled.h2`
   font-size: 1em;
   text-align: center;
-  color: gold;
+  color: #FFCC00;
   text-shadow:
    -0.5px -0.5px 0 #000,  
     0.5px -0.5px 0 #000,
@@ -27,12 +27,12 @@ export const SubTitle = styled.h2`
 export const MainTitle = styled.h1`
   font-size: 4em;
   text-align: center;
-  color: gold;
+  color: #FFCC00;
   text-shadow:
-   -1px -1px 0 #000,  
-    1px -1px 0 #000,
-    -1px 1px 0 #000,
-     1px 1px 0 #000;
+   -1.5px -1.5px 0 #0075BE,  
+    1.5px -1.5px 0 #0075BE,
+    -1.5px 1.5px 0 #0075BE,
+     1.5px 1.5px 0 #0075BE;
   position: fixed;
   top: 0;
   `;
